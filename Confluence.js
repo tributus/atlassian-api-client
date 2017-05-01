f3aafcd39ac38865fd1a887b158564f3940c8afd
Anderson Norberto Santos <anderson.santos@pcinformatica.com.br>
2017-05-01 18:26:49 -0300

    Major bugfixes

--- Confluence.js
+++ Confluence.js
@@ -17,32 +17,42 @@ module.exports = {
     },
     getInterface:function($this){
         return function(connection){
-            connection.protocol = connection.protocol || 'http';
-            connection.port = connection.port || '80';
-            connection.options = connection.options || {};
+            return new (function(connection){
+                connection.protocol = connection.protocol || 'http';
+                connection.port = connection.port || '80';
+                connection.options = connection.options || {};
 
-            this.get = function(url,params,data, success, fail){
-                $this.get(url,params,data,connection,success,fail);
-            };
-
-            this.post = function(url,params,data, success, fail){
-                $this.post(url,params,data,connection,success,fail);
-            };
-
-            this.getContentById = function(itemid,expand,success,fail){
-                var params ={
-                    itemid:itemid,
-                    expand:expand || ""
+                this.get = function(url,params,data, success, fail){
+                    $this.get(url,params,data,connection,success,fail);
                 };
-                $this.get("/rest/api/content/{itemid}?expand={expand}",params,undefined,connection,success,fail);
-            };
-            this.postContent = function(requestbody,success,fail){
-                $this.post("/rest/api/content",undefined,requestbody,connection,success,fail);
-            };
-            this.updatePageById = function(pageid,requestbody,success,fail){
-                $this.put("/rest/api/content/{pageid}",{pageid:pageid},requestbody,connection,success,fail);
-            };
-            return this;
+
+                this.post = function(url,params,data, success, fail){
+                    $this.post(url,params,data,connection,success,fail);
+                };
+                this.put = function(url,params,data, success, fail){
+                    $this.put(url,params,data,connection,success,fail);
+                };
+
+                this.delete = function(url,params,data, success, fail){
+                    $this.delete(url,params,data,connection,success,fail);
+                };
+
+                this.getContentById = function(itemid,expand,success,fail){
+                    var params ={
+                        itemid:itemid,
+                        expand:expand || ""
+                    };
+                    $this.get("/rest/api/content/{itemid}?expand={expand}",params,undefined,connection,success,fail);
+                };
+
+                this.postContent = function(requestbody,success,fail){
+                    $this.post("/rest/api/content",undefined,requestbody,connection,success,fail);
+                };
+                this.updatePageById = function(pageid,requestbody,success,fail){
+                    $this.put("/rest/api/content/{pageid}",{pageid:pageid},requestbody,connection,success,fail);
+                };
+            })(connection);
+            //return this;
         }
     }
 
--- Jira.js
+++ Jira.js
@@ -9,85 +9,99 @@ module.exports = {
     post:function(url,params,data, connectionData ,success, fail){
         request.postAtlassianData(request.buildApiUrl(params,url),data,connectionData,success,fail);
     },
+    put:function(url,params,data, connectionData ,success, fail){
+        request.putAtlassianData(request.buildApiUrl(params,url),data,connectionData,success,fail);
+    },
+    delete:function(url,params,data, connectionData ,success, fail){
+        request.deleteAtlassianData(request.buildApiUrl(params,url),data,connectionData,success,fail);
+    },
     getInterface:function($this){
         return function(connection){
-            connection.protocol = connection.protocol || 'http';
-            connection.port = connection.port || '80';
-            connection.options = connection.options || {};
-            connection.options.allowNoJsonResponse = connection.options.allowNoJsonResponse || false;
+            return new (function(connection){
+                connection.protocol = connection.protocol || 'http';
+                connection.port = connection.port || '80';
+                connection.options = connection.options || {};
+                connection.options.allowNoJsonResponse = connection.options.allowNoJsonResponse || false;
 
-            this.get = function(url,params,data, success, fail){
-                $this.get(url,params,data,connection,success,fail);
-            };
+                this.get = function(url,params,data, success, fail){
+                    $this.get(url,params,data,connection,success,fail);
+                };
 
-            this.post = function(url,params,data, success, fail){
-                $this.post(url,params,data,connection,success,fail);
-            };
-            this.getIssueByID = function(issueid,success,fail){
-                var params ={issueid:issueid};
-                $this.get("/rest/api/2/issue/{issueid}",params,undefined,connection,success,fail);
+                this.post = function(url,params,data, success, fail){
+                    $this.post(url,params,data,connection,success,fail);
+                };
+                this.put = function(url,params,data, success, fail){
+                    $this.put(url,params,data,connection,success,fail);
+                };
 
-            };
-            this.createIssue = function(issueData,success,fail){
-                $this.post("/rest/api/2/issue/",undefined,issueData,connection,success,fail);
-            };
+                this.delete = function(url,params,data, success, fail){
+                    $this.delete(url,params,data,connection,success,fail);
+                };
+                this.getIssueByID = function(issueid,success,fail){
+                    var params ={issueid:issueid};
+                    $this.get("/rest/api/2/issue/{issueid}",params,undefined,connection,success,fail);
 
-            this.linkIssues = function(linkTypeName,inwardIssueKey,outwardIssueKey,commentBody, success,fail){
-                var LinkRequestBody = {
-                    type: {
-                        name: linkTypeName
-                    }    ,
-                    inward: {
-                        key: inwardIssueKey
-                    },
-                    outwardIssue: {
-                        key: outwardIssueKey
+                };
+                this.createIssue = function(issueData,success,fail){
+                    $this.post("/rest/api/2/issue/",undefined,issueData,connection,success,fail);
+                };
+
+                this.linkIssues = function(linkTypeName,inwardIssueKey,outwardIssueKey,commentBody, success,fail){
+                    var LinkRequestBody = {
+                        type: {
+                            name: linkTypeName
+                        }    ,
+                        inward: {
+                            key: inwardIssueKey
+                        },
+                        outwardIssue: {
+                            key: outwardIssueKey
+                        }
+                    };
+                    if(commentBody){
+                        LinkRequestBody.comment ={body:commentBody};
                     }
+                    $this.post("/rest/api/2/issueLink",undefined,LinkRequestBody,connection,success,fail);
                 };
-                if(commentBody){
-                    LinkRequestBody.comment ={body:commentBody};
-                }
-                $this.post("/rest/api/2/issueLink",undefined,LinkRequestBody,connection,success,fail);
-            };
 
-            this.createRemoteLink = function(issueKeyOrID,url,title,success,fail){
-                var RequestBody = {object: {url:url,title:title}};
-                $this.post("/rest/api/latest/issue/{issuekey}/remotelink",{issuekey:issueKeyOrID},RequestBody,connection,success,fail);
-            };
-
-            this.addIssueComment = function(issueIdOrKey,comment, success,fail){
-                var requestBody = {"body": comment};
-                $this.post("/rest/api/2/issue/{issueIdOrKey}/comment",{issueIdOrKey:issueIdOrKey},requestBody,connection,success,fail);
-            };
-            this.changeIssueStatus = function(issueIdOrKey,transitionIDOrRequestBody,success,fail){
-                var reqParams = {
-                    issueIdOrKey:issueIdOrKey
+                this.createRemoteLink = function(issueKeyOrID,url,title,success,fail){
+                    var RequestBody = {object: {url:url,title:title}};
+                    $this.post("/rest/api/latest/issue/{issuekey}/remotelink",{issuekey:issueKeyOrID},RequestBody,connection,success,fail);
                 };
-                var requestBody = undefined;
-                if(typeof transitionIDOrRequestBody =="string"){
-                    requestBody = {
-                        "transition": {
-                            "id": transitionIDOrRequestBody
+
+                this.addIssueComment = function(issueIdOrKey,comment, success,fail){
+                    var requestBody = {"body": comment};
+                    $this.post("/rest/api/2/issue/{issueIdOrKey}/comment",{issueIdOrKey:issueIdOrKey},requestBody,connection,success,fail);
+                };
+                this.changeIssueStatus = function(issueIdOrKey,transitionIDOrRequestBody,success,fail){
+                    var reqParams = {
+                        issueIdOrKey:issueIdOrKey
+                    };
+                    var requestBody = undefined;
+                    if(typeof transitionIDOrRequestBody =="string"){
+                        requestBody = {
+                            "transition": {
+                                "id": transitionIDOrRequestBody
+                            }
                         }
                     }
-                }
-                else{
-                    requestBody = transitionIDOrRequestBody
-                }
-                jira.post("/rest/api/2/issue/{issueIdOrKey}/transitions?expand=transitions.fields",reqParams,requestBody,connection,success,fail)
-            };
-            this.getAllowedTransitions = function(issueIdOrKey,success,fail){
-                $this.get("/rest/api/latest/issue/{issueIdOrKey}?expand=transitions&fields=transitions",{issueIdOrKey:issueIdOrKey},undefined,connection,success,fail)
-            };
-            this.search = function(query,success,fail){
-                $this.get("/rest/api/2/search?jql={jql}",{jql:query},undefined,connection,success,fail);
-            };
+                    else{
+                        requestBody = transitionIDOrRequestBody
+                    }
+                    jira.post("/rest/api/2/issue/{issueIdOrKey}/transitions?expand=transitions.fields",reqParams,requestBody,connection,success,fail)
+                };
+                this.getAllowedTransitions = function(issueIdOrKey,success,fail){
+                    $this.get("/rest/api/latest/issue/{issueIdOrKey}?expand=transitions&fields=transitions",{issueIdOrKey:issueIdOrKey},undefined,connection,success,fail)
+                };
+                this.search = function(query,success,fail){
+                    $this.get("/rest/api/2/search?jql={jql}",{jql:query},undefined,connection,success,fail);
+                };
 
-            this.advancedSearch = function(requestBody,success,fail){
-                $this.post("/rest/api/2/search",undefined,requestBody,connection,success,fail);
-            };
-
-            return this;
+                this.advancedSearch = function(requestBody,success,fail){
+                    $this.post("/rest/api/2/search",undefined,requestBody,connection,success,fail);
+                };
+            })(connection);
+            //return this;
         }
     }
 };
