271d561e4b96d67d119a52be72a631c91a5062dc
Anderson Norberto Santos <anderson.norberto@totvs.com.br>
2019-05-29 11:35:59 -0300

    Added Oauth support

---
+++ WebRequestHelpers.Extensions.js
@@ -0,0 +1,180 @@
+var OAuth = require('oauth').OAuth;
+
+//consts
+var OAUTH_DEFAULT_VERSION           = "1.0";
+var OAUTH_DEFAULT_SIGNATURE_METHOD  = "RSA-SHA1";
+var OAUTH_DEFAULT_GET_CONTENT_TYPE  = "application/json";
+
+
+var getOauthConnector = function(connectionData){
+    var consumer = new OAuth(connectionData.OAuthParams.requestURL,
+        connectionData.OAuthParams.accessUrl,
+        connectionData.OAuthParams.consumerKey,
+        connectionData.OAuthParams.privateKeyData,
+        connectionData.OAuthParams.version || OAUTH_DEFAULT_VERSION,
+        connectionData.OAuthParams.authorizeCallBackURL,
+        connectionData.OAuthParams.signatureMethod || OAUTH_DEFAULT_SIGNATURE_METHOD
+    );
+
+    //the original "get" method was overriden here because in it's implementation
+    //it calls "_performSecureRequest" with a null value for "post_content_type"
+    //argument. When this argument is null, the value "application/x-www-form-urlencoded"
+    //is assumed. This behavior causes a 415 error when calling Jira APIS even when 
+    //content-type header is "application/json";
+    //This behavior was detected since v. 0.9.15 of oauth module
+    consumer.get = function(url,accessToken,acessTokenSecret,contentType,callback){
+        consumer._performSecureRequest(accessToken,acessTokenSecret,"GET",url,null,null,contentType||OAUTH_DEFAULT_GET_CONTENT_TYPE,callback);
+    }
+
+    return consumer; 
+}
+
+//module interface
+exports.unknownGET = function(apiUrl,bodyData,connectionData,success,fail){
+    return fail({connection:connectionData}, new Error("UNKOWN authentication method"));
+}
+
+exports.unknownCHANGE = function(method, apiUrl,data,connectionData,success,fail){
+    return fail({connection:connectionData}, new Error("UNKOWN authentication method"));
+}
+
+exports.BasicAuthenticationGET = function(apiUrl,bodyData,connectionData,success,fail){
+    var options = {
+        host: connectionData.host,
+        port: connectionData.port,
+        path: apiUrl,
+        headers:{
+            "Authorization": "Basic " + Buffer.from(connectionData.userName +":" + connectionData.password).toString('base64')
+        }
+    };
+    var protocol = require(connectionData.protocol);
+    protocol.get(options, function(resp){
+        var data = '';
+        resp.on('data', function(chunk){
+            data = data + chunk.toString();
+        });
+        resp.on("end", function(){
+            try{
+                var objData = JSON.parse(data);
+                return success(objData);
+            }
+            catch(ex){
+                if(connectionData.options.allowNoJsonResponse){
+                    return success(data,ex);
+                }
+                else{
+                    return fail(data,ex);
+                }
+            }
+
+        });
+    }).on("error", function(e){
+        return fail(e);
+    });
+}
+
+exports.BasicAuthenticationCHANGE = function(method, apiUrl,data,connectionData,success,fail){
+    method = method || "POST";
+    var api = apiUrl;
+    var body = JSON.stringify(data||{});
+    var options = {
+        host: connectionData.host,
+        port:  connectionData.port,
+        path: api,
+        method:method,
+        headers:{
+            "X-Atlassian-Token":" no-check",
+            "content-type": "application/json",
+            "Content-Length": Buffer.byteLength(body),
+            'Accept': 'application/json',
+            "Authorization": "Basic " + Buffer.from(connectionData.userName +":" + connectionData.password).toString('base64')
+        }
+    };
+    var post_req = require(connectionData.protocol).request(options, function(resp){
+        var data = '';
+        resp.on('data', function(chunk){
+            data = data + chunk.toString();
+        });
+        resp.on("end", function(){
+            try{
+                if(data){
+                    var jsonData = JSON.parse(data);
+                    return success(jsonData);
+                }
+                else{
+                    return success({status:"success", tag:"void-response"});
+                }
+            }
+            catch(ex){
+                if(connectionData.options.allowNoJsonResponse){
+                    success(data,ex);
+                }
+                else{
+                    return fail(ex,data);
+                }
+            }
+        });
+        resp.on("error", function(e){
+            return fail(e);
+        })
+    });
+    post_req.end(body);
+}
+
+exports.OAuthGET = function(apiUrl,bodyData,connectionData,success,fail){
+    var consumer = getOauthConnector(connectionData);
+    var fullUrl = connectionData.host + apiUrl;
+    
+    consumer.get(fullUrl,connectionData.oauthAccessToken,connectionData.oauthAccessTokenSecret,"application/json",
+        function(error,data){
+            if(error) return fail(error);
+            try{
+                if(data){
+                    var jsonData = JSON.parse(data);
+                    return success(jsonData);
+                }
+                else{
+                    return success({status:"success", tag:"void-response"});
+                }
+            }
+            catch(ex){
+                if(connectionData.options.allowNoJsonResponse){
+                    success(data,ex);
+                }
+                else{
+                    return fail(ex,data);
+                }
+            }
+        }
+    );
+}
+
+exports.OAuthCHANGE = function(method, apiUrl,data,connectionData,success,fail){
+    var consumer = getOauthConnector(connectionData);
+    var fullUrl = connectionData.host + apiUrl;
+    
+    method = method || "POST";
+    var body = JSON.stringify(data||{});
+    consumer._performSecureRequest(connectionData.oauthAccessToken,connectionData.oauthAccessTokenSecret,method,fullUrl,null,body,OAUTH_DEFAULT_GET_CONTENT_TYPE,
+        function(error,data){
+            if(error) return fail(error);
+            try{
+                if(data){
+                    var jsonData = JSON.parse(data);
+                    return success(jsonData);
+                }
+                else{
+                    return success({status:"success", tag:"void-response"});
+                }
+            }
+            catch(ex){
+                if(connectionData.options.allowNoJsonResponse){
+                    success(data,ex);
+                }
+                else{
+                    return fail(ex,data);
+                }
+            }
+        }
+    );
+}
--- WebRequestHelpers.js
+++ WebRequestHelpers.js
@@ -1,9 +1,9 @@
 /**
  * Created by anderson.santos on 28/04/2017.
  */
+var extensions = require('./WebRequestHelpers.Extensions');
 var impl = {
     buildApiUrl:function(data,url){
-
         if(data){
             var keys = url.match(/\{[a-zA-Z]+}/g);
             if(keys){
@@ -20,88 +20,16 @@ var impl = {
             return url;
         }
     },
+    resolveAuthMethod : function(connectionData){
+        if(connectionData.oauthAccessToken && connectionData.oauthAccessTokenSecret) return "OAuth";
+        if(connectionData.userName && connectionData.password) return "BasicAuthentication";
+        return 'unknown';
+    },
     getAtlassianData: function(apiUrl,bodyData,connectionData,success,fail){
-        var options = {
-            host: connectionData.host,
-            port: connectionData.port,
-            path: apiUrl,
-            headers:{
-                "Authorization": "Basic " + new Buffer(connectionData.userName +":" + connectionData.password).toString('base64')
-            }
-        };
-        var protocol = require(connectionData.protocol);
-        protocol.get(options, function(resp){
-            var data = '';
-            resp.on('data', function(chunk){
-                data = data + chunk.toString();
-            });
-            resp.on("end", function(){
-                try{
-                    var objData = JSON.parse(data);
-                    return success(objData);
-                }
-                catch(ex){
-                    if(connectionData.options.allowNoJsonResponse){
-                        return success(data,ex);
-                    }
-                    else{
-                        console.log(data);
-                        return fail(data,ex);
-                    }
-                }
-
-            });
-        }).on("error", function(e){
-            return fail(e);
-        });
+        return extensions[impl.resolveAuthMethod(connectionData) + 'GET'](apiUrl,bodyData,connectionData,success,fail);
     },
     changeAtlassianData:function(method, apiUrl,data,connectionData,success,fail){
-        method = method || "POST";
-        var api = apiUrl;
-        var body = JSON.stringify(data||{});
-        var options = {
-            host: connectionData.host,
-            port:  connectionData.port,
-            path: api,
-            method:method,
-            headers:{
-                "X-Atlassian-Token":" no-check",
-                "content-type": "application/json",
-                "Content-Length": Buffer.byteLength(body),
-                'Accept': 'application/json',
-                "Authorization": "Basic " + new Buffer(connectionData.userName +":" + connectionData.password).toString('base64')
-            }
-        };
-        var post_req = require(connectionData.protocol).request(options, function(resp){
-            var data = '';
-            resp.on('data', function(chunk){
-                data = data + chunk.toString();
-            });
-            resp.on("end", function(){
-                try{
-                    if(data){
-                        var jsonData = JSON.parse(data);
-                        return success(jsonData);
-                    }
-                    else{
-                        return success({status:"success", tag:"void-response"});
-                    }
-                }
-                catch(ex){
-                    if(connectionData.options.allowNoJsonResponse){
-                        success(data,ex);
-                    }
-                    else{
-                        console.log(ex,data);
-                        return fail(ex,data);
-                    }
-                }
-            });
-            resp.on("error", function(e){
-                return fail(e);
-            })
-        });
-        post_req.end(body);
+        return extensions[impl.resolveAuthMethod(connectionData) + 'CHANGE'](method,apiUrl,data,connectionData,success,fail);
     },
     postAtlassianData:function(apiUrl,data,connectionData,success,fail){
         impl.changeAtlassianData("POST",apiUrl,data,connectionData,success,fail);
